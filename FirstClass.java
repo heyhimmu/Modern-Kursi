c1b171fd7652275c33e54498c0f99ef8746ed2aa
heyhimmu <himuskharola99@gmail.com>
2025-06-01 14:39:47 +0530

    First Commit

---
+++ FirstClass.java
@@ -0,0 +1,7 @@
+public class FirstClass{
+    public static void main(String[] args){
+        System.out.print("hi");
+        System.out.println("hello");
+        System.out.print("how r you?");
+    }
+}
