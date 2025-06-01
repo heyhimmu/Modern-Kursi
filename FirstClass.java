838784beb81861c657617f87e841b3c02044a39f
heyhimmu <himuskharola99@gmail.com>
2025-06-01 14:48:25 +0530

    update changes

--- FirstClass.java
+++
@@ -1,7 +0,0 @@
-public class FirstClass{
-    public static void main(String[] args){
-        System.out.print("hi");
-        System.out.println("hello");
-        System.out.print("how r you?");
-    }
-}
